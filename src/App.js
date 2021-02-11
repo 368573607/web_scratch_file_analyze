de35311ae2efff31fc6f14e5a048017c9bb0fa2b
redsun <xuhuading@foxmail.com>
2021-02-11 13:26:22 +0800

    添加业务文件
    任有Bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 const React = require("react");
+const analyzefile = require("scratch_file_analyze");
 
 class App extends React.Component {
-    render() {
-      return (
-        <h1>Test page.</h1>
-      );
+    constructor(props) {
+        super(props);
+
+        this.state = { value: "{}", jsonvalue: {}, result: {} };
+
+        this.handleChange = this.handleChange.bind(this);
     }
-  }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+        this.setState({ jsonvalue: JSON.parse(this.state.value) });
+        this.analyze();
+    }
+
+    analyze() {
+        analyzefile(this.state.jsonvalue).then((resu) => {
+            this.setState({ result: resu });
+        }).catch((err) => {
+            this.setState({ result: err });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Scratch分析器</h1>
+                <textarea value={this.state.value} onChange={this.handleChange} />
+                <p>
+                    解析结果：
+                    {JSON.stringify(this.state.result)}
+                </p>
+            </div>
+        )
+    }
+}
 
 module.exports = App;
